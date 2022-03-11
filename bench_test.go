6dd9068a38ba4ded54f8a7e31a6dd2ac2b20afb4
Oskars Lielāmurs <oskarslielamurs@gmail.com>
2022-03-11 16:23:37 +0200

    Add concurrent get testing

--- bench_test.go
+++ bench_test.go
@@ -2,6 +2,8 @@ package sgcache
 
 import (
 	"fmt"
+	"strconv"
+	"sync"
 	"testing"
 	"time"
 )
@@ -14,4 +16,29 @@ func BenchmarkCache(b *testing.B) {
 		cache.Set(fmt.Sprint(n%1000000), "value", time.Second*3)
 		cache.Get(fmt.Sprint(n % 1000000))
 	}
+}
+
+func BenchmarkConcurrentGet(b *testing.B) {
+	b.StopTimer()
+	n := 10000
+	c := New(time.Duration(time.Second), 900000000)
+	keys := make([]string, n)
+	for i := 0; i < n; i++ {
+		k := "foo" + strconv.Itoa(i)
+		keys[i] = k
+		c.Set(k, "bar", time.Second*3)
+	}
+	each := b.N / n
+	wg := new(sync.WaitGroup)
+	wg.Add(n)
+	for _, v := range keys {
+		go func(k string) {
+			for j := 0; j < each; j++ {
+				c.Get("foo")
+			}
+			wg.Done()
+		}(v)
+	}
+	b.StartTimer()
+	wg.Wait()
 }
